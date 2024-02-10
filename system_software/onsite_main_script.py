0a1e61d46b0eb0a7e970793c3eb8c54c3b8ad840
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-10 12:42:13 -0700

    Adding onsite_main_script.py

---
+++ system_software/onsite_main_script.py
@@ -0,0 +1,35 @@
+#Header
+#
+#
+# Import Packages and Libraries 
+import sys, os # maybe not os
+#import write, read and close file?
+
+###########################################
+# Functions 
+
+def calibration_cycle():
+# for n in range(0,N)
+# 	if n = 0 send back longitude and latitude 
+#		get longitude and latitude from NMEA file 
+# 	else: send back battery health, temp and check inbox
+#		get battery health 
+# 		get temperature 
+# 		check inbox
+#		if message, execute commands
+#			call command interpreter 
+#       else:
+# 			continue
+# 	 sleep(5) #maybe less cause it would take a few seconds to run code
+
+###########################################
+# Main function 
+
+# Check mailbox 
+
+# if in calibration mode, run every 5 mins until n = N (so it stops in >1.5 hours)
+	calibration_cycle()
+# else: normal ops
+#	
+
+
