e2f55aa80626421a556dff58fe3de7e6dd7657cc
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-11 14:56:59 -0700

    Update python_input_args.py

---
+++ Unix_Bash_and_Python_Guides/python_input_args.py
@@ -0,0 +1,11 @@
+# Guide/outline for python script that takes input arguments
+
+# Imports and libraries 
+import os
+
+# Functions 
+
+
+
+# Main function (not necessarily needed, it just can make things look cleaner)
+def main(): 
