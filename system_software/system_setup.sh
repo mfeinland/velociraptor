ef5d4e06bff8e8048a2dcec7c516acffc0ddb8fd
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-11 14:41:43 -0700

    Update system_setup.sh

--- system_software/system_setup.sh
+++ system_software/system_setup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # set up env variables
+# either run 'export VAR="variable"' (probably not cause its only valid for the current shell and its child processes)
+# or have them all in a text file 
 
 # set up connections 
 
+
 # get time from GNSS
+utc_time=$(python path/get_time.py)
+
+# add leap seconds: TAI = UTC+37 as of Feb 2024
+# leap seconds obtained from Bulletin C located at https://www.iers.org/IERS/EN/Publications/Bulletins/bulletins.html
+
 
 # set system time
+
+sudo date -s "@$utc_time"
