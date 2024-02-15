ac4922803fccd4d3fa6c7b2ddaf3ff22ae89f62e
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-15 15:32:14 -0700

    Update system_setup.sh

--- system_software/system_setup.sh
+++ system_software/system_setup.sh
@@ -5,7 +5,7 @@
 # or have them all in a text file 
 
 # set up connections 
-python3 devSetup.py input
+python3 devSetup.py
 
 
 # get time from GNSS
@@ -14,6 +14,7 @@ utc_time=$(python path/get_time.py)
 # set system time
 
 sudo date -s "@$utc_time"
+# "12 FEB 2024 00:00:00"
 
 # add leap seconds: TAI = UTC+37 as of Feb 2024
 # leap seconds obtained from Bulletin C located at https://www.iers.org/IERS/EN/Publications/Bulletins/bulletins.html
