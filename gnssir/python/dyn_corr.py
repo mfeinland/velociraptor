6e52f9d5f2f2801dc302fe85592aa4cc0671739e
clairewadman <157850274+clairewadman@users.noreply.github.com>
2024-02-22 09:04:44 -0700

    Update dyn_corr.py

--- gnssir/python/dyn_corr.py
+++ gnssir/python/dyn_corr.py
@@ -15,19 +15,22 @@ def dyn_corr(hs, ts, es):
     # go through all data points
     for n in range(length):
         point = n + 1
-        dt = ts[point+1] - ts[point] # dt is the user specified temporal resolution, or time between antenna height calculations
+        dt = ts[point+1] - ts[point] # dt is the sampling frequency
         e_dots.append((es[point+1]-es[point-1])/(2*dt)) # numerical differentiation to get time rate of change of e
-        # e_dots.append(1) # numerical differentiation to get time rate of change of e
         A[n][0] = 1
         A[n][1] = (np.tan(es[point]))/e_dots[n] # ASSUMING THAT E_DOT IS IN RADS/TIME
-    print('A = \n', A)
-    print('hs = \n', hs[1:len(hs)-1])
-    # linalg.solve is the function of NumPy to solve a system of linear scalar equations 
-    H_array = np.linalg.solve(A, hs) # this will give us [H, H_dot] and what we need is H
+    print('size of A:', A.shape)
+    print('e_dots = \n', e_dots)
+    hs = hs[1:len(hs)-1]
+    print('size of hs: ', hs.shape)
+    print('hs = \n', hs)
+    H_array, residuals, rank, singular_values = np.linalg.lstsq(A, hs, rcond=None)
     return H_array
         
 ts = [0.0, 500.0, 1000.0, 1500.0, 2000.0, 2500.0, 3000.0, 3500.0] # times of data points [s]
-hs = [12.00, 12.01, 12.02, 12.03, 12.01, 12.02, 12.01] # antenna heights [m]
-es = [0.52, 0.78, 1.04, 1.57, 2.09, 2.35, 2.61, 3.0] # elevation angles [rad]
+hs = np.array([12, 14, 16, 25, 22, 19, 14, 10]) # antenna heights [m]
+hs = hs.reshape(8,1)
+es = [0.52, 0.78, 1.04, 1.57, 2.09, 2.35, 2.61, 2.70, 3.0] # elevation angles [rad]
 
 H = dyn_corr(hs, ts, es)
+print('H = ', H)
