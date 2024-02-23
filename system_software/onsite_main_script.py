0b8e2658a8898b96d81040c8f3414f7a0bfa07ab
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-02-23 14:43:07 -0700

    Update onsite_main_script.py

--- system_software/onsite_main_script.py
+++ system_software/onsite_main_script.py
@@ -16,7 +16,7 @@
 #   []
 #
 # Limitations: 
-#    
+#   
 #
 # Future edits: 
 #   [issue]                               [status]                    [name]
@@ -28,113 +28,124 @@
 import re
 import sys, os # maybe not os if we turn cmd interpreter into function
 from common_functions import read_file, write_file
+import time
+from receiver_functions import read_nmea, cereal_func
 #from ops_functions import calibration_cycle, sys_health, check_mail
 
-###########################################
+###############################################################################
 # Functions 
 
 def calibration_cycle(ser, bat_level, temperature):
-	   N = 18 # 18*5 = 90 mins
-	   for n in range(0,N)
-        if n = 0: # send back longitude, latitude, battery health, and sys temp
-			         # get longitude and latitude from NMEA file 
-			         flag = 0
-			         while flag == 0: # flag is down
-				            # read in nmea lines
-				            data = ser.readline()
-				            # if line contains long/lat (GLL -- Geographic Position - Longitude/Latitude)
-				            check = re.search("GLL", data)
-				            if check:
-					               flag = 1 # flag goes up
-					               GLL_line = data.split(", ")
-					               latitude = GLL_line[1] + GLL_line[2]
-					               longitude = GLL_line[3] + GLL_line[4]
+	N = 18 # 18*5 = 90 mins
+	for n in range(0,N):
+		if n == 0: # send back longitude, latitude, battery health, and sys temp
+			# get longitude and latitude from NMEA file 
+			flag = 0
+			while flag == 0: # flag is down
+				# read in nmea lines
+				data = ser.readline()
+				# if line contains long/lat (GLL -- Geographic Position - Longitude/Latitude) GGA
+				check = re.search("GGA", data)
+				if check:
+					flag = 1 # flag goes up
+					GGA_line = data.split(", ")
+					latitude = GGA_line[2] + GGA_line[3]
+					longitude = GGA_line[4] + GGA_line[5]
 
-            # send message 
-		          message = "long=" + str(longitude) + ",lat=" + str(latitude) + ",B=" + bat_level + ",T=" + temperature
-            # send_string
-		          send_string(message)
+            		# send message 
+					message = "long=" + str(longitude) + ",lat=" + str(latitude) + ",B=" + bat_level + ",T=" + temperature
+            		# send_string
+					send_string(message)
 													
-	       else:
-            # check inbox
-			         command = check_mail()
-			
-			         if command = no_message: # execute commands
-				            sleep(5)
-			         else:
-   	            os.system("python command_interpreter.py " + command)
-		              sleep(5)
+		else:
+            	# check inbox
+			command = check_mail()
+			if command == "no_message": # execute commands
+				time.sleep(5)
+			else:
+				os.system("python command_interpreter.py " + command)
+				time.sleep(5)
 
-def check_mail():
+#def send_string(message):
+#	print(message)
+
+#def check_mail():
 	# check inbox
+#	bla = 1
 	
 def sys_health():
-    message = []
+	message = []
 	   # get battery level [units]
-	   bat_level = 30
+	bat_level = 30
    	# get system temperature [deg. C]
-	   temperature = 20
+	temperature = 20
 
-	   # check battery level 
-	   if bat_level <= 10: # no idea what value or units this should be
-		  message.append("Battery level too low. Pausing ops for 2 hrs")
+	# check battery level 
+	if bat_level <= 10: # no idea what value or units this should be
+		message.append("Battery level too low. Pausing ops for 2 hrs")
 	
-	   # check system temperature 
-	   if temperature < -30: # degrees Celcius
-		  message.append("System temp. is too hot. Pausing ops for 2 hrs")
-	   elif temperature > 55: # degrees Celsius 
-		  message.append("System temp. is too cold. Pausing ops for 2 hrs")
+	# check system temperature 
+	if temperature < -30: # degrees Celcius
+		message.append("System temperature too hot. Pausing ops for 2 hrs")
+	elif temperature > 55: # degrees Celsius 
+		message.append("System temperature too cold. Pausing ops for 2 hrs")
 		
-	   # send messages back if needed 
-	   if len(message[0]) > 1:
-		      for item in message 
-        send_string(item)
-        sys.exit() # if there are any issues (might change this for cold temp)
+	# send messages back if needed 
+	if len(message[0]) > 1:
+		for item in message:
+			send_string(item)
+			sys.exit() # if there are any issues (might change this for cold temp)
 	
-    return bat_level, temperature
+	return bat_level, temperature
 
 ###########################################
 # Main function 
 def main():
-	
-    ser = os.environ.get("SERIAL")
-    if ser == "None"
-        ser = cereal()
-	
-    set_vars = read_file('setvars.txt')
-    freq = set_vars[0]
-    el_mask = [set_vars[1], set_vars[2]]
-    az_mask = [set_vars[3], set_vars[4]]
-    mode = set_vars[5]
-    timeres = set_vars[6]
+	filename = "dino.csv"
+	dataAmount = 20000
+	#ser = os.environ.get("SERIAL")
+	#if ser == "None":
+	ser = cereal_func() #os.system(cereal()) #cereal.py 
+	ser = "None"
+
+	set_vars = read_file('setvars.txt')
+	freq = set_vars[0]
+	el_mask = [9,17] #[set_vars[1], set_vars[2]]
+	az_mask = [270, 360] #[set_vars[3], set_vars[4]]
+	mode = "Normal" #set_vars[5]
+	temporal_res = set_vars[6]
 
     # Check mailbox 
-    command = check_mail() # 0 if no mail
+	#command = check_mail() # 0 if no mail
     # if command != 0:
-    os.system("python command_interpreter.py " + command)
+	#os.system("python command_interpreter.py " + command)
     # or make this a function: command_interpreter(command)
 
-    # check system health 
-    bat_level, temperature = sys_health()
+	# check system health 
+	#bat_level, temperature = sys_health()
+	
+	bat_level = 20
+	temperature = 100
+	
+	# if in calibration mode, run every 5 mins until n = N (so it stops in >1.5 hours)
+	if mode == "calibration":
+		calibration_cycle(ser, bat_level, temperature)
 
-    # if in calibration mode, run every 5 mins until n = N (so it stops in >1.5 hours)
-    if mode == "calibration":
-				    calibration_cycle(ser, bat_level, temperature)
-
-    else: #normal ops (need to implement time resolution)
+	else: #normal ops (need to implement time resolution)
     #	time res idea: read in all available files? divide number of files by 
     # 	number of desired blocks  
-        read_nmea()
-		      nmea2dino()
-		      heights = reflector_height()
+		read_nmea(ser, dataAmount)
+
+		nmea2dino("nmea.txt")
+		heights = reflector_height(filename, az_mask[0], az_mask[1], el_mask[0], el_mask[1])
     
         # check system health 
-	       bat_level, temperature = sys_health()
+		#bat_level, temperature = sys_health()
     
         # send string to ground station 
-								message = "B=" + bat_level + ",T=" + temperature + "H=" + heights[0]
-	       send_string(message)
-        if len(heights) > 2
+		#message = "B=" + bat_level + ",T=" + temperature + "H=" + heights[0]
+		message = "H=" + heights[0]
+		send_string(message)
 
 if __name__ == "__main__":
     main()
