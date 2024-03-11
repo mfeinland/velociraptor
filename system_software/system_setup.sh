2c825234464ab7fc943a77631e8c50b2a9237e68
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-11 13:47:40 -0600

    Update system_setup.sh

--- system_software/system_setup.sh
+++ system_software/system_setup.sh
@@ -1,32 +1,18 @@
 #!/bin/bash
 
-# set up env variables
-# either run 'export VAR="variable"' (probably not cause its only valid for the current shell and its child processes)
-# or have them all in a text file 
-
 # set up connections and make the serial connection a enviromental variable
-export SERIAL=$(python3 devSetup.py)
-#echo 'export NEW_VAR="Hello, New Session!"' >> ~/.bashrc
-#source ~/.bashrc
-#echo $NEW_VAR
-
-# Output:
-# Hello, New Session!
-
-# dmesg | grep "now attached" 
+usb_connections=$(dmesg | grep "now attached") 
 # cp210x (GNSS receiver)
 # FTDI (transceiver)
 
+python path/devSetup.py $usb_connections
 
 # get time from GNSS
 utc_time=$(python path/get_time.py)
 
-# set system time
-
+# set system time "12 FEB 2024 00:00:00"
 sudo date -s "@$utc_time"
-# "12 FEB 2024 00:00:00"
 
 # add leap seconds: TAI = UTC+37 as of Feb 2024
 # leap seconds obtained from Bulletin C located at https://www.iers.org/IERS/EN/Publications/Bulletins/bulletins.html
-
 sudo date -s "37 seconds"
