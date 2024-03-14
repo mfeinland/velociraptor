b4c76a3a205719e938a31ce9ad30cf432fb0bc26
clairewadman <157850274+clairewadman@users.noreply.github.com>
2024-03-14 16:00:25 -0600

    Update send_MT.py

--- gui/send_MT.py
+++ gui/send_MT.py
@@ -3,21 +3,26 @@
 
 # in terminal before running: python -m pip install requests
 import requests
+import numpy as np
 # query parameters
     # it would be a good idea to have these as inputs at the beginning so a different user could set up their RockBlock with our code
 imei = "300434068462010"
 username1 = "max.feinland%40colorado.edu"
-username2 = "max.feinland@colorado.edu" # this is just how they did the example code I'm not sure why
+username2 = "max.feinland@colorado.edu" # this is just how they did the RockBlock example code I'm not sure why
 password = "Velociraptor"
-msg_ASCII = "Hi" # need to write this so we can input an ASCII message and then send it in hex
-msg_hex = "1A" # must be written as hex-encoded byte array
+msg_ASCII = "Wutz up" # user input will be string
 
+# convert user input to hex (this is required by RockBlock)
+ascii_bytes = msg_ASCII.encode('ascii')
+hex_list = [format(byte, '02x') for byte in ascii_bytes]
+msg_hex = ''
+for n in range(len(hex_list)):
+    msg_hex = msg_hex+hex_list[n]
+
+# url format required by RockBlock
 url = 'https://rockblock.rock7.com/rockblock/MT?imei=300434068462010&username='+username1+'&username='+username2+'&password='+password+'&data='+msg_hex
 
-
-headers = {"accept": "text/plain"}
-
-response = requests.post(url, headers=headers)
+headers = {"accept": "text/plain"} # also how they did the RockBlock example code
 # Response body will be list of values separated by commas
 # first value: status ("OK" or "FAILED")
 # if status "OK":
@@ -25,6 +30,5 @@ response = requests.post(url, headers=headers)
 # if status "FAILED"
     # second value: integer error code
     # third value: error code text description
-
+response = requests.post(url, headers=headers)
 print(response.text)
-
