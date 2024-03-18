6c002a1460eaf011de1f292fb83a98d6fb7f7977
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-17 19:09:08 -0600

    Update system_setup.sh

--- system_software/system_setup.sh
+++ system_software/system_setup.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# set up connections and make the serial connection a enviromental variable
+# Determine the USB connections
 usb_connections=$(dmesg | grep "now attached") 
 # cp210x (GNSS receiver)
 # FTDI (transceiver)
 
-python path/devSetup.py $usb_connections
-
 # get time from GNSS
-utc_time=$(python path/get_time.py)
+utc_time=$(python path/system.py $usb_connections)
 
 # set system time "12 FEB 2024 00:00:00"
 sudo date -s "@$utc_time"
 
 # add leap seconds: TAI = UTC+37 as of Feb 2024
-# leap seconds obtained from Bulletin C located at https://www.iers.org/IERS/EN/Publications/Bulletins/bulletins.html
+# leap seconds obtained from Bulletin C located at 
+# https://www.iers.org/IERS/EN/Publications/Bulletins/bulletins.html
 sudo date -s "37 seconds"
