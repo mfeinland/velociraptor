47379039be352ffd395d2c84747cda6cafaeb6cd
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-19 21:28:45 -0600

    Update system_setup.py

--- system_software/system_setup.py
+++ system_software/system_setup.py
@@ -16,7 +16,7 @@ def main():
     arg =' '.join(sys_args[1:])
     usb_connections = arg.split("[") #print(stuff) #usb_connections = stuff[1:] #print(usb_connections)
 
-    path = '/home/velociraptor/raptor_test/'
+    path = '/home/velociraptor/two_hour_lifecycle_test/'
     USBs = ['None','None']
 
     # set mode to calibration
@@ -30,21 +30,20 @@ def main():
 
             if re.search("cp210x",thing[0]): # cp210x (GNSS receiver)
                 print("cp210x is connected to ttyUSB" + thing[1])
-                USBs[0] = thing[1].strip() # GNSS_usb
-                GNSS_ser = serial.Serial("/dev/ttyUSB" + USBs[0], 115200)
+                USBs[0] = int(thing[1].strip()) # GNSS_usb
 
             if re.search("FTDI",thing[0]): # FTDI (transceiver)
                 print("FTDI is connected to ttyUSB" + thing[1])
-                USBs[1] = thing[1].strip() # TRX_usb
-                TRX_ser = serial.Serial('/dev/ttyUSB' + USBs[1],  19200)
+                USBs[1] = int(thing[1].strip()) #TRX_usb
 
     # get time from GNSS
+    GNSS_ser = serial.Serial("/dev/ttyUSB" + USBs[0], 115200)
     t = get_time(GNSS_ser) 
 
     write_file(USBs, path + 'serial_connections.txt')
     # send_string("Connection between RPI and TRX is successful")
 
-    return t
+    sys.exit(t)
 
 if __name__ == "__main__":
     main()
