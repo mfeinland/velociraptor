5432e83abe5d030cd9c934cf59495dc36093ccbc
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-22 01:13:04 -0600

    Update rockBLOCK_functions.py

--- two_hour_lifecycle_test/rockBLOCK_functions.py
+++ two_hour_lifecycle_test/rockBLOCK_functions.py
@@ -53,35 +53,61 @@ def connect_to_rock(TRX_ser):
     check = ahh_out.decode('utf-8').rstrip()
     return check
 
+
 def send_string(current_string, TRX_ser):
-    # establish serial connection 
-    # TRX_ser = serial.Serial("/dev/ttyUSB0", 19200)
+    import serial
+    import time
     
+    # establish serial connection 
+    ser = TRX_ser
+
     # At the top of the hour, check the inbox for messages.
-    # Add a line here that checks the time. 
-      # - this would actually be in the main script - becca
+    # Add line(s) here that check(s) the time. (Becca?)
     establish_connection = "AT\r"
-    TRX_ser.write(establish_connection.encode("ascii"))
-    ans = TRX_ser.readline()
-    if ans == "OK": # if the serial connection is successful
-        successful_transmission = 0
-        while successful_transmission == 0:
-            write_msg = "AT+SBDWT=" + current_string + "\r" # request read/write status
-            TRX_ser.write(write_msg)
-            sbdix_ans1 = TRX_ser.readline()
-            if sbdix_ans1 == "OK":
-                TRX_ser.write("AT+SBDIX\r")
-                sbdix_ans = TRX_ser.readline()
-                sbdix_ans = sbdix_ans[8:].split(",") # splits response up by commas
+    ser.write(establish_connection.encode("ascii"))
+    establish_connection_input = ser.readline()
+    # print(establish_connection_input.decode('utf-8').rstrip()) # this should say "AT"
+    
+    establish_connection_out = ser.readline()
+    establish_connection_out = establish_connection_out.decode('utf-8').rstrip()
+    # print(establish_connection_out) # this should say "OK"
+    
+    if establish_connection_out == "OK": # if the serial connection is successful
+        print("success!")
+        successful_transmission = 0 # assume the message has not yet successfully transmitted.
+        
+        write_msg = "AT+SBDWT=" + current_string + "\r" # request read/write status
+        ser.write(write_msg.encode("ascii")) # write the message to the rockblock
+        sbdwt_in = ser.readline() # should say the contents of write_msg
+        sbdwt_out = ser.readline() # should say OK
+        if sbdwt_out.decode('utf-8').rstrip() == "OK":
+            number_of_attempts = 0
+            while successful_transmission == 0 and number_of_attempts < 10:
+                w = "AT+SBDIX\r" # start the transmission
+                ser.write(w.encode("ascii"))
+                if number_of_attempts > 0:
+                    sbdix_0 = ser.readline()
+                    sbdix_1 = ser.readline()
+                    sbdix_in = ser.readline()
+                else:
+                    sbdix_in = ser.readline()
                 
+                sbdix_in = sbdix_in.decode('utf-8').rstrip()
+                sbdix_out = ser.readline()
+                sbdix_out = sbdix_out.decode('utf-8').rstrip()
+                sbdix_out = sbdix_out[8:].split(",") # splits response up by commas
+                number_of_attempts += 1
                 # Detailed in the AT command reference
-                mo_status = sbdix_ans[0] # should be 0-2
+                mo_status = int(sbdix_out[0]) # should be 0-2
             
                 # Ok this is all stuff to read commands from the ground station
-                if int(mo_status) < 3: # if the message was successfully transmitted
+                if mo_status < 3: # if the message was successfully transmitted
                     successful_transmission += 1
                 else:
-                    time.sleep(10) # wait 10 seconds to try again
+                    # print("Transmission unsuccessful, waiting 10 seconds\n")
+                    time.sleep(20) # wait 20 seconds to try again
+    if number_of_attempts > 10:
+        print("Maximum attempts exceeded")
 
 def check_mail(TRX_ser):
     message = []
@@ -96,7 +122,7 @@ def check_mail(TRX_ser):
       mailbox_status = mailbox_status.split(',')
       # OK\r
 
-      if ans[3] == 1: 
+      if mailbox_status[3] == 1: 
           # Transfer "Hello1" message to your controller 
           TRX_ser.write("AT+SBDRT\r")
           message_end = 0 # flag down
