f6418cdd1bff1697c81f6d381fa3141099427c48
BeccaBlum <157848286+BeccaBlum@users.noreply.github.com>
2024-03-30 08:10:24 -0600

    Update test.sh

--- what_is_on_rpi/test.sh
+++ what_is_on_rpi/test.sh
@@ -3,8 +3,8 @@
 #export TESTVARY="Hello, I'm the test variable"
 #echo $TESTVARY
 
-#python /home/velociraptor/raptor_test/test.py
+python /home/velociraptor/two_hour_lifecycle_test/test.py
 
-sudo date -s "17 Mar 2024 20:40:00"
+#sudo date -s "17 Mar 2024 20:40:00"
 
 #echo "done"
